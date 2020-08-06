d731e377ab2df29889126db0c7d31f71114c2ac0
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-06 23:14:37 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
       }
     }
 
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
       }
     }
 
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,6 +58,7 @@ def dennis_ritchies_language
       }
     }
 
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -82,6 +85,12 @@ def adding_matz
       }
     }
 
+  programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages: ["LISP", "C"]
+  }
+  
+  programmer_hash
 end
 
 def changing_alan
@@ -103,6 +112,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  
+  programmer_hash
 
 end
 
@@ -125,5 +138,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 
+  programmer_hash
 end
